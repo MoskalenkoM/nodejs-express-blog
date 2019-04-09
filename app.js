b32a337080a912530cdd2fd6e80071d7bd9608c6
Maxim Moskalenko <maksm.84@gmail.com>
2019-04-09 21:45:26 +0300

    Add first model in DB

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-const data = [`Привет`, `Мир`, `JavaScript`];
 
-app.get('/', (req, res) => res.render('index', { data: data }));
+app.get('/', (req, res) => {
+  Post.find({}).then(posts => {
+    res.render('index', { posts });
+  });
+});
 
 app.get('/create', (req, res) => res.render('create'));
 app.post('/create', (req, res) => {
-  data.push(req.body.text);
+  const { title, body } = req.body;
+
+  Post.create({
+    title,
+    body
+  }).then(post => console.log(post.id));
+
   res.redirect('/');
 });
 
